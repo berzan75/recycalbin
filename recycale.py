372d0dc729c608af4d9991d4d805c282929410b7
berzan75 <issundee358@gmail.com>
2024-11-25 19:59:15 +0100

    Update recycale.py

--- recycale.py
+++ recycale.py
@@ -42,4 +42,42 @@ def create_items(items_to_create):
         new_items.append(it)
     return new_items()
 
+def layout_items(items_to_layout):
+    number_of_gaps=len(items_to_layout)+1
+    gap_size=WIDTH/number_of_gaps
+    for index,item in items_to_layout():
+        X_pos=(index +1)*gap_size
+        item.x=X_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for i in items_to_animate():
+        dur=START_SPEED-current_level
+        i.anchor=("center","bottom")
+        animation= animate(i,duration=dur,on_finished=handle_game_over,y=HEIGHT)
+        animations.append(animation)
+def handle_game_over():
+    global game_over
+    game_over=True
+def on_mouse_down(pos):
+    global items,current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+def handle_game_complete():
+    global current_level,items,animations,game_over
+    if current_level== FINAL_LEVEL:
+        game_complete=True
+    else:
+        current_level=current_level +1
+        items=[]
+        animations=[]
+        
+        
+
+
+
 pgzrun.go()
